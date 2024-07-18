ff99599ca30672d5fb83ffd3ba0622851089536e
pathumck <ckpathumck@gmail.com>
2024-07-18 16:03:05 +0530

    update code for deserialization

--- src/Test.java
+++ src/Test.java
@@ -10,7 +10,7 @@ public class Test {
         //Serialization
 
         try {
-            //Saving Object in a file
+            //Saving object in a file
             FileOutputStream fileOutputStream = new FileOutputStream(fileName);
 
             //objectOutputStream object is create with the fileOutputStream object
@@ -26,6 +26,34 @@ public class Test {
 
         } catch (IOException e) {
             System.out.println("IO Exception is caught");
+        }
+
+        Obj objectTwo = null;
+
+        //Deserialization
+
+        try {
+            //Read bytes from a file specified
+            FileInputStream fileInputStream = new FileInputStream(fileName);
+
+            //Create objectInputStream object that used to
+            // deserialize objects from the byte stream provided by the FileInputStream.
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+            //Reads an object from the ObjectInputStream and casts it to the type Obj.
+            objectTwo = (Obj) objectInputStream.readObject();
+
+            objectInputStream.close();
+            fileInputStream.close();
+
+            System.out.println("Object has been deserialized");
+            System.out.println("a = " + objectTwo.getA());
+            System.out.println("b = " + objectTwo.getB());
+
+        } catch (IOException e) {
+            System.out.println("IOException is caught");
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException is caught");
         }
     }
 }
