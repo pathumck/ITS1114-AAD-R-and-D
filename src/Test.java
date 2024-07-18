90850c16a38fca41f4d3e1c16517fb3d48a1984b
pathumck <ckpathumck@gmail.com>
2024-07-18 14:36:26 +0530

    update code for serialization

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,31 @@
 package src;
 
+import java.io.*;
+
 public class Test {
     public static void main(String[] args) {
+        Obj objOne = new Obj(1,"IJSE");
+        String fileName = "file.ser";
 
+        //Serialization
+
+        try {
+            //Saving Object in a file
+            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+
+            //objectOutputStream object is create with the fileOutputStream object
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+            //converts the objOne into a byte stream
+            objectOutputStream.writeObject(objOne);
+
+            objectOutputStream.close();
+            fileOutputStream.close();
+
+            System.out.println("Object has been serialized");
+
+        } catch (IOException e) {
+            System.out.println("IO Exception is caught");
+        }
     }
 }
