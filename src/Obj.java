6c7a0b42b072a83604bbe32299ffd1b8a1e6eecc
pathumck <ckpathumck@gmail.com>
2024-07-18 14:13:05 +0530

    create Obj.java class

---
+++ src/Obj.java
@@ -0,0 +1,35 @@
+package src;
+
+class Obj {
+    private int a;
+    private String b;
+
+    public Obj(int a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    @Override
+    public String toString() {
+        return "Obj{" +
+                "a=" + a +
+                ", b='" + b + '\'' +
+                '}';
+    }
+}
